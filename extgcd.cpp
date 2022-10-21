b9f13b7d978b7325f6db5027fdf2057c89a2a8f7
systy257 <88966531+systy257@users.noreply.github.com>
2022-10-21 22:42:16 +0300

    Update extgcd.cpp

--- extgcd.cpp
+++ extgcd.cpp
@@ -1,14 +1,13 @@
 int extgcd(int a, int b) {
-	int aa = 1, ab = 0, ba = 0, bb = 1;
-	while (b != 0) {
-		int d = a / b;
-		a %= b;
-		aa -= ba * d;
-		ab -= bb * d;
-		swap(aa, ba);
-		swap(ab, bb);
-		swap(a, b);
-	}
- 
-	return aa;
+    int aa = 1, ab = 0, ba = 0, bb = 1;
+    while (b != 0) {
+        int d = a / b;
+        a %= b;
+        aa -= ba * d;
+        ab -= bb * d;
+        swap(aa, ba);
+        swap(ab, bb);
+        swap(a, b);
+    }
+    return aa;
 }
